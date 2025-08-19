79fa5315474fcf8bddad1e902f03ede54bc477f3
Marrior-dot <mrlimaroberto@gmail.com>
2025-08-19 13:11:57 -0300

    encoding applied

--- src/services/microsoftAuth.ts
+++ src/services/microsoftAuth.ts
@@ -361,7 +361,7 @@ export const microsoftAuthService = {
 
   async checkM365Email(email: string): Promise<ServiceResponse<MicrosoftAuthData>> {
     try {
-      const response = await fetch(`${AUTH_ENDPOINT}/check-user/${email}`, {
+      const response = await fetch(`${AUTH_ENDPOINT}/check-user/${encodeURIComponent(email)}`, {
         method: 'GET',
         headers: {
           'Content-Type': 'application/json', // Recommended if expecting JSON
